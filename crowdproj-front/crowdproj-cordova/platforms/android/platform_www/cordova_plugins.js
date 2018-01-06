0a4b80c93373ab27757edb7ef0e9bec104712062
Sergey Okatov <sokatov@gmail.com>
2018-01-06 11:38:30 +0300

    Update Cordova Android to 7.0; Fix: out of memory

--- crowdproj-front/crowdproj-cordova/platforms/android/platform_www/cordova_plugins.js
+++ crowdproj-front/crowdproj-cordova/platforms/android/platform_www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2"
+  "cordova-plugin-console": "1.0.7",
+  "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
