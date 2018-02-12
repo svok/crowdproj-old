e5e9cce5f97594158dd903e27aa6f9f8e06b137d
Sergey Okatov <sokatov@gmail.com>
2018-02-12 10:15:25 +0300

    Preparing for publication

---
+++ README.md
@@ -0,0 +1,15 @@
+## CrowdProj microservices framework
+
+This is an experimental framework for building microswervice architecture
+applications. It is build on the following stack of other frameworks and
+libraries.
+
+1. Spring Boot 2 framework is used for building web-gateway between
+backend and frontend applications.
+
+2. Apache Kafka message queue system is used for communications between
+microservices.
+
+3. With the Apache Flink stream data processor we build microservices.
+
+4. Angular5 framework is used for frontend application building.
--- crowdproj-front/crowdproj-angular/src/app/modules/user/services/websocket.service.ts
+++ crowdproj-front/crowdproj-angular/src/app/modules/user/services/websocket.service.ts
@@ -1,3 +1,21 @@
+/**
+ *
+ * Copyright © 2017 Sergey Okatov. All rights reserved.
+ * Author: Sergey Okatov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Injectable }                           from "@angular/core";
 import { Subject, Observable, Subscription }    from 'rxjs/Rx';
 import { WebSocketSubject }                     from "rxjs/observable/dom/WebSocketSubject";
@@ -25,7 +43,7 @@ export class WebSocketService {
 
     public sendMessage(message: RequestInterface): void {
         let str: string = message.stringify();
-        console.log("sendins a message: " + str);
+        console.log("sending a message: " + str);
         this.ws.next(str);
     }
 
@@ -48,7 +66,9 @@ export class WebSocketService {
                 this.message.next(MessageFactory.build(data));
             },
 
-            error: () => {
+            error: (err: any) => {
+                console.log("websocket error");
+                console.log(err);
 
                 self.opened.next(false);
                 this.message.next(new ClosedMessage());
